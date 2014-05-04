b7e8fa0d8f1ab34e4e30fc4e0546b187b24a3652
ya790206 <ya790206@yahoo.com.tw>
2014-05-04 11:56:21 +0800

    add example how to build single executable jar.

---
+++ build_single_jar/JarExample.java
@@ -0,0 +1,6 @@
+public class JarExample {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
---
+++ build_single_jar/README.md
@@ -0,0 +1,4 @@
+Use below command to build single executable jar.
+``` bash
+jar cfm jarexample.jar jexample.mf *.class
+```
